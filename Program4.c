40aed9ce35677fe3b9d94dc05706178b6c513dde
PrasadYeole11 <120306210+PrasadYeole11@users.noreply.github.com>
2023-02-09 16:13:53 +0530

    Add files via upload

---
+++ Program4.c
@@ -0,0 +1,45 @@
+/* 1.Accept number of rows and columns from user and Display below Pattern.
+Input: iRow=3 iCol=4
+       * # * #
+       * # * #
+       * # * #
+*/
+
+#include<stdio.h>
+
+void Display(int iRow,int iCol)
+ {
+     int i=0,j=0;
+     printf("The pattern you want to print is:\n");
+     
+     for(i=1;i<=iRow;i++)
+      {
+         for(j=1;j<=iCol;j++)
+          {
+             if(j%2==0)
+              { 
+                printf("#\t");
+              } 
+             else
+              { 
+                printf("*\t");
+              } 
+          }
+         printf("\n");
+      }          
+ } 
+
+int main()
+ {
+    int nRow=0,nCol=0;
+    
+    printf("Enter the number of rows\n");
+    scanf("%d",&nRow);
+    
+    printf("Enter the number of columns\n");
+    scanf("%d",&nCol);
+    
+    Display(nRow,nCol);
+    return 0;
+ }
+
